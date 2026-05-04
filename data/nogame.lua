b282bcdfb9708f0c9d7dd905527845931c73c65a
GuglioIsStupid <guglioisstupid@users.noreply.github.com>
2026-05-04 12:47:33 -0400

    Setup project

--- data/nogame.lua
+++
@@ -1,9 +0,0 @@
-local love = require("love")
-
-function love.nogame()
-    -- TODO: Make this pretty?
-    function love.draw()
-        love.graphics.setColor(1, 1, 1)
-        love.graphics.print("No game found. Please add a main.lua file to sd:/LOVEPower/game/", 10, 10)
-    end
-end
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,7 @@
-#define SOL_ALL_SAFETIES_ON 1
-#ifndef USE_LUAJIT
-    #define SOL_LUAJIT 0
-#endif
-
-#include "love/love.hpp"
+#include <iostream>
 
 int main(int argc, char** argv) {
-    try {
-        return love::initialize(argc, argv);
-    } catch (const std::exception &e) {
-        love::logError(std::string("Fatal exception: ") + e.what());
-    } catch (...) {
-        love::logError("Fatal unknown exception");
-    }
-    return -1;
+    std::cout << "Hello world" << std::endl;
+    
+    return 0;
 }
